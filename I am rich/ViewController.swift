6c6b3271405479fec84229a75a11c69513ed8212
tommy huang <tommy.huang@onelab.tw>
2024-05-23 14:32:40 +0800

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by tommy.huang on 2024/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
